a0a7d7b560fb16756aeb601812cf0cd96330470c
Francis Morkeh Mensah(10xFrancis) <40639664+Francismensah@users.noreply.github.com>
2023-08-17 13:28:17 +0000

    Update boot.sh

--- deploy-scripts/boot.sh
+++ deploy-scripts/boot.sh
@@ -2,4 +2,4 @@
 
 echo "Running new container"
 DOCKER_IMAGE=calebackom/roberta-api
-docker run -d -p --name roberta-backend 8185:8185 $DOCKER_IMAGE
+docker run -dp 8185:8185 --name roberta-backend $DOCKER_IMAGE
